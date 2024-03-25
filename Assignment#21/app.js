e77099bd71bd1f2dee995f495e616ca9f374c11e
maviazindani <maviazindaninfo@gmail.com>
2024-03-26 02:56:48 +0500

    work done

---
+++ Assignment#21/app.js
@@ -0,0 +1,63 @@
+// Chapter numbner 21-25
+
+// Question number 1
+// var firstName = prompt('Enter your First name');
+// var secondName = prompt('Enter your Second name');
+
+// document.write("My fullname is "+firstName+secondName);
+
+// Question number 2
+// var fullName = prompt('Enter your full name');
+
+// document.write("My fullname is"+firstName+secondName)
+// document.write(fullName.length)
+
+// Question number 3
+// var contryName =prompt('Enter your contry name');
+// var indexValue =prompt('Enter string to find its index number');
+// var indexNumber = contryName.indexOf(indexValue)
+
+// document.write("String: "+contryName+'<br>')
+// document.write('Index of '+"'"+indexValue+"': "+indexNumber)
+
+// Question number 4
+// var word =prompt('Enter a word');
+// var findLetter =prompt('Enter string to find its index number');
+// var gotLetter = word.lastIndexOf(findLetter)
+
+// document.write("String: "+word+'<br>')
+// document.write('Index of '+"'"+findLetter+"': "+gotLetter)
+
+
+// Question number 5
+// var word1 =prompt('Enter a word');
+// var findCharacter =prompt('Enter a number to find its string value');
+// var gotCharacter = word1.charAt(findCharacter)
+
+// document.write("String: "+word1+'<br>')
+// document.write('Index of '+"'"+findCharacter+"': "+gotCharacter)
+
+
+// Question number 6
+// var firstName = prompt('Enter your First name');
+// var secondName = prompt('Enter your Second name');
+// var fullName = firstName.concat(" "+secondName);
+// document.write("My fullname is "+ fullName );
+
+
+// Question number 7
+var para = "Ali and Sami are best friends. They play cricket and football together.";
+
+if (para.indexOf() == "and") {
+    
+}
+// Question number 1
+// Question number 1
+// Question number 1
+// Question number 1
+// Question number 1
+// Question number 1
+// Question number 1
+// Question number 1
+// Question number 1
+// Question number 1
