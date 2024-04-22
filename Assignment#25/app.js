f6c0b9f601de28317d4cd219219bb0aa901e0b17
maviazindani <maviazindaninfo@gmail.com>
2024-04-23 03:22:51 +0500

    25 Assingment done

--- Assignment#25/app.js
+++ Assignment#25/app.js
@@ -1,6 +1,13 @@
 // calculater
 
 const disply = document.getElementById("display");
+var userinput1 ;
+var oprater ;
+var userinput2 ;
+var result ;
+
+
+
 function getvalue(input){
   disply.value += input;
 };
@@ -14,10 +21,45 @@ function clearDisplay(del){
 }
 
 function getopr(){
-  try{
-    disply.value = eval(disply.value);
-  }catch(error){
-    disply.value = "error";
-  }
-}
+   for (var i =0; i < disply.value.length; i++) {
+       console.log();
+    if (disply.value[i] === "+" || disply.value[i] === "-" || disply.value[i] === "/" || disply.value[i] === "*" || disply.value[i] === "Squ" || disply.value[i] === "+") {
+      userinput1 = parseFloat(disply.value.slice(0,i));
+      console.log("userinput1: " + userinput1);
+      
+      var oprater = disply.value[i];
+      console.log("Oprater: " + oprater)
 
+      userinput2 = parseFloat(disply.value.slice(i + 1));
+      console.log("userinput2: " + userinput2)
+
+      break;
+    }
+    if (result === null) {
+      result = disply.value;
+    }
+    
+   }
+   switch (oprater) {
+    case oprater = "+":
+      result = userinput1 + userinput2;
+      break;
+    case oprater = "-":
+        result = userinput1 - userinput2;
+        break;
+    case oprater = "*":
+      result = userinput1 * userinput2;
+      break;
+    case oprater = "/":
+        result = userinput1 / userinput2;
+        break;
+    case oprater = "Squ":
+      result = userinput1 * userinput1;
+      break;
+    default:
+      result = "error";
+      break;
+   }
+   disply.value = result;
+
+  }
